6ca235ff43582e0af82201fc65a40e861b2a9664
cpatelss90 <charvin.patel@smoothstack.com>
2021-05-03 01:25:50 -0500

    commit

---
+++ utopiaTraveller/src/main/java/com/ss/utopia/UtopiaTravellerApplication.java
@@ -0,0 +1,13 @@
+package com.ss.utopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtopiaTravellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtopiaTravellerApplication.class, args);
+	}
+
+}
